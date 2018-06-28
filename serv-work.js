e85ffacfee7bdd89f18b9f113f945a183f473504
wilfort <wilfort.stephane@hotmail.fr>
2018-06-28 17:36:16 +0200

    fin confilt du contenu

--- serv-work.js
+++ serv-work.js
@@ -31,7 +31,8 @@ var urlsToCache = [
   '/becode-AHOD2/assent/js/script.js',
   '/becode-AHOD2/assent/js/snippet.js',
   '/becode-AHOD2/icon192.png',
-  '/becode-AHOD2/icon512.png'
+  '/becode-AHOD2/icon512.png',
+  '/becode-AHOD2/serv-work.js'
 ];
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
